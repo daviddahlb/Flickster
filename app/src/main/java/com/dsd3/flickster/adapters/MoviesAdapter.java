dcb6169df7be46557b13a0069ce0af2aab3f7b9b
daviddahlb <davidsdahlberg@gmail.com>
2019-02-10 22:08:10 -0800

    Implemented onClick that activates MovieDetail activity

--- app/src/main/java/com/dsd3/flickster/adapters/MoviesAdapter.java
+++ app/src/main/java/com/dsd3/flickster/adapters/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.dsd3.flickster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -9,9 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.dsd3.flickster.DetailActivity;
 import com.dsd3.flickster.R;
 import com.dsd3.flickster.models.Movie;
 
@@ -55,15 +58,17 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        RelativeLayout container;
 
         public ViewHolder(View itemView){
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             //put data inside the TextView text items
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
@@ -73,6 +78,15 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
                 imageUrl = movie.getBackdropPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, DetailActivity.class);
+                    intent.putExtra("title", movie.getTitle());
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 }
